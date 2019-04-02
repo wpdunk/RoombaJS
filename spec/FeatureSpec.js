71ea6e6fbc98b2da933e4d8606e89bf3260c10e1
William Dunk <williamdunk@gmail.com>
2019-04-02 10:09:06 +0100

    :sparkles: add ability to drive hoover north

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -7,4 +7,11 @@ describe("Features", function() {
     hoover.run("./spec/support/testOne.txt");
     expect(console.log).toHaveBeenCalledWith("0 0\n0");
   });
+
+  it("given single driving instruction, should return updated position", function() {
+    hoover = new Hoover();
+    spyOn(console, "log");
+    hoover.run("./spec/support/testTwo.txt");
+    expect(console.log).toHaveBeenCalledWith("0 1\n0");
+  });
 });
--- src/Hoover.js
+++ src/Hoover.js
@@ -9,8 +9,14 @@ class Hoover {
       .readFileSync(filename)
       .toString()
       .split("\n");
-    this.hooverPosition = inputArray[1];
-    console.log(`${this.hooverPosition}\n${this.dirtSum}`);
+    this.hooverPosition = inputArray[1].split(" ").map(Number);
+    var directionsArray = inputArray[2].split("");
+    directionsArray.forEach(move => {
+      if (move === "N") {
+        this.hooverPosition[1] += 1;
+      }
+    });
+    console.log(`${this.hooverPosition.join(" ")}\n${this.dirtSum}`);
   }
 }
 
