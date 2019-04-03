6b279815e435d687b943671ba7b56a3d15eff177
William Dunk <williamdunk@gmail.com>
2019-04-03 10:49:43 +0100

    :recycle: add describe and helper beforeEach to improve clarity of tests

--- spec/HooverSpec.js
+++ spec/HooverSpec.js
@@ -1,59 +1,64 @@
 const Hoover = require("../src/hoover");
 
 describe("Hoover", function() {
-  it("can record initial position from input", function() {
-    hoover = new Hoover();
-    hoover.readInput("./spec/support/testOne.txt");
-    expect(hoover.hooverPosition).toEqual([0, 0]);
-  });
-  it("can record driving instructions from input", function() {
-    hoover = new Hoover();
-    hoover.readInput("./spec/support/testTwo.txt");
-    expect(hoover.directionsArray).toEqual(["N"]);
-  });
-  it("can record dirt locations from input", function() {
-    hoover = new Hoover();
-    hoover.readInput("./spec/support/testFour.txt");
-    expect(hoover.dirtArray).toEqual([[0, 1]]);
-  });
-  it("can record room dimensions from input", function() {
-    hoover = new Hoover();
-    hoover.readInput("./spec/support/testOne.txt");
-    expect(hoover.roomDimensions).toEqual([5, 5]);
+  describe("readInput", function() {
+    beforeEach(function() {
+      hoover = new Hoover();
+    });
+
+    it("can record initial position from input", function() {
+      hoover.readInput("./spec/support/testOne.txt");
+      expect(hoover.hooverPosition).toEqual([0, 0]);
+    });
+
+    it("can record driving instructions from input", function() {
+      hoover.readInput("./spec/support/testTwo.txt");
+      expect(hoover.directionsArray).toEqual(["N"]);
+    });
+
+    it("can record dirt locations from input", function() {
+      hoover.readInput("./spec/support/testFour.txt");
+      expect(hoover.dirtArray).toEqual([[0, 1]]);
+    });
+
+    it("can record room dimensions from input", function() {
+      hoover.readInput("./spec/support/testOne.txt");
+      expect(hoover.roomDimensions).toEqual([5, 5]);
+    });
   });
 
-  it("can drive north", function() {
-    hoover = new Hoover();
-    hoover.roomDimensions = [2, 2];
-    hoover.hooverPosition = [1, 1];
-    hoover.directionsArray = ["N"];
-    hoover.drive();
-    expect(hoover.hooverPosition).toEqual([1, 2]);
+  describe("drive", function() {
+    beforeEach(function() {
+      hoover = new Hoover();
+      hoover.roomDimensions = [2, 2];
+      hoover.hooverPosition = [1, 1];
+    });
+
+    it("can drive north", function() {
+      hoover.directionsArray = ["N"];
+      hoover.drive();
+      expect(hoover.hooverPosition).toEqual([1, 2]);
+    });
+
+    it("can drive east", function() {
+      hoover.directionsArray = ["E"];
+      hoover.drive();
+      expect(hoover.hooverPosition).toEqual([2, 1]);
+    });
+
+    it("can drive south", function() {
+      hoover.directionsArray = ["S"];
+      hoover.drive();
+      expect(hoover.hooverPosition).toEqual([1, 0]);
+    });
+
+    it("can drive west", function() {
+      hoover.directionsArray = ["W"];
+      hoover.drive();
+      expect(hoover.hooverPosition).toEqual([0, 1]);
+    });
   });
-  it("can drive east", function() {
-    hoover = new Hoover();
-    hoover.roomDimensions = [2, 2];
-    hoover.hooverPosition = [1, 1];
-    hoover.directionsArray = ["E"];
-    hoover.drive();
-    expect(hoover.hooverPosition).toEqual([2, 1]);
-  });
-  it("can drive south", function() {
-    hoover = new Hoover();
-    hoover.roomDimensions = [2, 2];
-    hoover.hooverPosition = [1, 1];
-    hoover.directionsArray = ["S"];
-    hoover.drive();
-    expect(hoover.hooverPosition).toEqual([1, 0]);
-  });
-  it("can drive west", function() {
-    hoover = new Hoover();
-    hoover.roomDimensions = [2, 2];
-    hoover.hooverPosition = [1, 1];
-    hoover.directionsArray = ["W"];
-    hoover.drive();
-    expect(hoover.hooverPosition).toEqual([0, 1]);
-  });
+
   it("can record count of dirt cleaned", function() {
     hoover = new Hoover();
     hoover.roomDimensions = [2, 2];
