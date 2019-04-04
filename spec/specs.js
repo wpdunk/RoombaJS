03cba43548bae8b926e37d3b02ba8a8a395214f9
William Dunk <williamdunk@gmail.com>
2019-04-04 16:59:20 +0100

    :white_check_mark: improve visibility of tests summary

---
+++ spec/specs.js
@@ -0,0 +1,31 @@
+// setup Jasmine
+const Jasmine = require("jasmine");
+const jasmine = new Jasmine();
+jasmine.loadConfig({
+  spec_dir: "spec",
+  spec_files: ["**/*[sS]pec.js"],
+  helpers: ["helpers/**/*.js"],
+  random: false,
+  seed: null,
+  stopSpecOnExpectationFailure: false
+});
+jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 15000;
+
+// setup console reporter
+const JasmineConsoleReporter = require("jasmine-console-reporter");
+const reporter = new JasmineConsoleReporter({
+  colors: 1, // (0|false)|(1|true)|2
+  cleanStack: 1, // (0|false)|(1|true)|2|3
+  verbosity: 4, // (0|false)|1|2|(3|true)|4|Object
+  listStyle: "indent", // "flat"|"indent"
+  timeUnit: "ms", // "ms"|"ns"|"s"
+  timeThreshold: { ok: 500, warn: 1000, ouch: 3000 }, // Object|Number
+  activity: true,
+  emoji: true, // boolean or emoji-map object
+  beep: true
+});
+
+// initialize and execute
+jasmine.env.clearReporters();
+jasmine.addReporter(reporter);
+jasmine.execute();
